31ae162b06a5c406cc3ea7af1fea8974268efde4
bu6hunt3r <bu6hunt3r@web.de>
2021-06-16 13:32:15 +0200

    Made it possible to activate desktop profile

---
+++ .config/guix/manifests/desktop.scm
@@ -0,0 +1,30 @@
+(specifications->manifest
+ '(
+   
+   ;; Settings Manager
+   "xsettingsd"
+   
+   ;; GTK Themes
+   "arc-icon-theme"
+   "matcha-theme"
+   "hicolor-icon-theme"
+   "gnome-icon-theme"
+   "gnome-backgrounds"
+   "papirus-icon-theme"
+   "breeze-icons"
+   
+   ;; Fonts
+   "font-fira-code"
+   "font-jetbrains-mono"
+   "font-iosevka"
+   "font-iosevka-aile"
+   "font-abattis-cantarell"
+   "font-dejavu"
+   "font-google-noto"
+   "font-gnu-freefont"
+   "font-liberation"
+   "font-awesome"
+   "font-google-material-design-icons"
+   "gs-fonts"
+   
+))
